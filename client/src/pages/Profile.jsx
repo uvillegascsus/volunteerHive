93dea6d1a351125e2517b2c3565e84104e9552a9
Laibacodes <122822872+Laibacodes@users.noreply.github.com>
2026-05-14 14:18:06 -0700

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { useAuth } from '../conext/AuthContext';
+import { useAuth } from '../context/AuthContext';
 
 export default function Profile() {
     const { user, updateUser, API } = useAuth();
